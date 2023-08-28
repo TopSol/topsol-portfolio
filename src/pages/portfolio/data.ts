d927eb6cf067df09b9c7fe38bdba1c37347c4515
maramzan <arsalanramzan811@gmial.com>
2023-08-28 15:59:29 +0500

    compleat about us page and start the portfolio page

---
+++ src/pages/portfolio/data.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    name: "All",
+  },
+  {
+    id: 2,
+    name: "Mobile App",
+  },
+  {
+    id: 3,
+    name: "Web App",
+  },
+  {
+    id: 4,
+    name: "Desktop App",
+  },
+];
