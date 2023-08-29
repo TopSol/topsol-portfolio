01084e0a6102fa8c777adf0ebb97db79dfcaae0c
maramzan <arsalanramzan811@gmial.com>
2023-08-29 15:00:15 +0500

    compleat the portfolio and services Pages

---
+++ src/components/smallFooter.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PrimaryBtn from './PrimaryBtn'
+
+export default function smallFooter() {
+  return (
+    <div className="bg-gradient-to-b mt-[100px]  from-secondary to-primary py-[100px] ">
+        <div className="md:container mx-auto flex flex-col md:flex-row  justify-between items-center    " >
+        <div className=" md:w-[60%]">
+          <h1 className="font-bold text-[40px]  md:text-[50px] lg:text-[70px] font-montserrat text-white mx-5  " >Ready to discuss your project</h1>
+        </div>
+        <div className="mt-6 md:w-[30%]" >
+          <PrimaryBtn text="Start your project" additionalClasses="  bg-transparent   border-white border-2 text-white hover:bg-primary " />
+        </div>
+        </div>
+      </div>
+  )
+}
---
+++ src/pages/portfolioDetail/data.ts
@@ -0,0 +1,30 @@
+export const dataList = [
+  {
+    id: 1,
+    item: "A Robust Native App (Android + iOS)",
+  },
+  {
+    id: 2,
+    item: "Visually Impressive App Interface",
+  },
+  {
+    id: 3,
+    item: "A Powerful Admin Dashboard",
+  },
+  {
+    id: 4,
+    item: "Seamless Onboarding for Authorized Service Professionals",
+  },
+  {
+    id: 5,
+    item: "Real-Time Chat Connectivity Between Consumers and Providers",
+  },
+  {
+    id: 6,
+    item: "Secure Payment Gateway Integration",
+  },
+  {
+    id: 7,
+    item: "Push Notifications",
+  },
+];
