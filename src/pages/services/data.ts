afed85561e9ae20cbc39dc4b16ad60a5b21ad571
maramzan <arsalanramzan811@gmial.com>
2023-08-29 15:00:15 +0500

    compleat the portfolio and services Pages

---
+++ src/pages/services/data.ts
@@ -0,0 +1,52 @@
+export const data = [
+    {
+      id: 1,
+      heading: "Green Tea Studios, developed by our experienced team",
+      detail:
+        "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+    },
+    {
+      id: 2,
+      heading: "Green Tea Studios, developed by our experienced team",
+      detail:
+        "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+    },
+    {
+      id: 3,
+  
+      heading: "Green Tea Studios, developed by our experienced team",
+      detail:
+        "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+    },
+    {
+      id: 4,
+      heading: "Green Tea Studios, developed by our experienced team",
+      detail:
+        "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+    },
+    {
+        id: 5,
+        heading: "Green Tea Studios, developed by our experienced team",
+        detail:
+          "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+      },
+      {
+        id: 6,
+        heading: "Green Tea Studios, developed by our experienced team",
+        detail:
+          "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+      },
+      {
+        id: 7,
+    
+        heading: "Green Tea Studios, developed by our experienced team",
+        detail:
+          "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+      },
+      {
+        id: 8,
+        heading: "Green Tea Studios, developed by our experienced team",
+        detail:
+          "Qorem ipsum dolor sit amet, consectetur adipiscing elit.Qorem ipsum dolor sit amet,consectetur adipiscing elit.QoQorem ipsum dolor sit amet, consectetur adipiscing elit.rem ",
+      },
+  ];
