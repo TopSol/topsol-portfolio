7682fe94601e75fb41931bf73022ce2e49f54fbd
maramzan <arsalanramzan811@gmail.com>
2023-08-31 16:50:51 +0500

    Resolved conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby";
-import Navbar from "../components/Navbar";
-import Footer from "../components/footerSection";
-import Hero from "../components/hero";
-import CardSection from "../components/cardSection";
-import ServicesOffers from "../components/servisesOffers";
-import TopSolExperts from "../components/topSolExpert";
-import OurPortfolio from "../components/ourPortfolio";
-import RatingSection from "../components/ratingSection";
-import Awards from "../components/awards";
-import GetInTouch from "../components/getInTouch";
+import type { HeadFC, PageProps } from "gatsby"
+import Navbar from "../components/Navbar"
+import Footer from "../components/footerSection"
+import Hero from "../components/hero"
+import CardSection from "../components/cardSection"
 
 
 const IndexPage: React.FC<PageProps> = () => {
@@ -18,12 +12,6 @@ const IndexPage: React.FC<PageProps> = () => {
       <Navbar/>
       <Hero/>
       <CardSection/>
-      <ServicesOffers/>
-      <TopSolExperts/>
-      <OurPortfolio/>
-      <RatingSection/>
-      <Awards/>
-      <GetInTouch/>
       <Footer/>
     </div>
   )
