d3eed7448ba863a8c879dbfb614869434f3ccc6e
Hamza-Khurshid <hamzakhurshid5509@gmail.com>
2023-09-12 11:37:04 +0500

    fetch portfolio from firebase

--- src/pages/portfolioDetail/index.tsx
+++ src/pages/portfolioDetail/index.tsx
@@ -2,26 +2,23 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import SmallFooter from "../../components/smallFooter";
 import Footer from "../../components/footerSection";
-import PrimaryBtn from "../../components/PrimaryBtn";
-import check1 from "../../images/check1.png";
 import Hero from "./component/Hero";
 import PortfolioDetailBody from "./component/portfolioDetailBody";
-import { dataList } from "./data";
 import { db } from "../../utils/firebase";
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
-import { useParams } from "@reach/router";
 
 export default function index() {
   const [detail, setDetail] = useState({});
 
-  const params = useParams();
+  const url = window.location.href;
+
+  const urlObj = new URL(url);
+
+  const id = urlObj.searchParams.get("id");
 
   const fetchPortfolioItem = async () => {
     try {
-      const portfolioItemRef = doc(
-        collection(db, "portFolio"),
-        "I3fJPL3xbGZn07cAMcIg"
-      );
+      const portfolioItemRef = doc(collection(db, "portFolio"), id);
       const portfolioItemDoc = await getDoc(portfolioItemRef);
 
       if (portfolioItemDoc.exists()) {
@@ -34,13 +31,11 @@ export default function index() {
     }
   };
 
-  console.log(detail, "asdasdasdasd");
-
   useEffect(() => {
-    // if (id) {
-    fetchPortfolioItem();
-    // }
-  }, []);
+    if (id) {
+      fetchPortfolioItem();
+    }
+  }, [id]);
   return (
     <div>
       <Navbar />
