89c044b188c92da49be920186f6b001e7b80a0a5
Asif-chohan <rmasifchohan@gmail.com>
2023-09-19 14:45:34 +0500

    add careersection card image on hover

--- src/pages/careers/components/recruitmentProcess/allOpenings/index.tsx
+++ src/pages/careers/components/recruitmentProcess/allOpenings/index.tsx
@@ -41,19 +41,36 @@ function AllOpenings() {
 
   return (
     <div className="bg-gradient-to-b from-primary-gradientOne to-primary-gradientTwo">
-      <div className="md:container md:mx-auto flex justify-center items-center flex-col">
+      <div className="md:container md:mx-auto sm:mx-0 mx-[14px] flex justify-center items-center flex-col">
         <div className="flex flex-col items-center justify-center mb-14">
           <h1 className="text-primary-white md:text-[40px] text-3xl text-center font-semibold md:mt-[49px] mt-[52px] ">
             All Openings
           </h1>
           <div className="flex justify-center items-center">
-            <LineAnimation width='178px' height='7px' backgroundColor='#FFFFFF' marginTop="22px" />
+            <LineAnimation
+              width="178px"
+              height="7px"
+              backgroundColor="#FFFFFF"
+              marginTop="22px"
+            />
           </div>
         </div>
         <div className="w-[95%]  md:mb-[39px]  ">
           <div className="flex  bg-white rounded-lg py-[20px] px-[20px] ">
-            <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none" >
-              <path d="M15.5833 8.5C19.4953 8.5 22.6667 11.6713 22.6667 15.5833M23.6 23.5944L29.75 29.75M26.9167 15.5833C26.9167 21.8426 21.8426 26.9167 15.5833 26.9167C9.3241 26.9167 4.25 21.8426 4.25 15.5833C4.25 9.3241 9.3241 4.25 15.5833 4.25C21.8426 4.25 26.9167 9.3241 26.9167 15.5833Z" stroke="#00B8F1" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              fill="none"
+            >
+              <path
+                d="M15.5833 8.5C19.4953 8.5 22.6667 11.6713 22.6667 15.5833M23.6 23.5944L29.75 29.75M26.9167 15.5833C26.9167 21.8426 21.8426 26.9167 15.5833 26.9167C9.3241 26.9167 4.25 21.8426 4.25 15.5833C4.25 9.3241 9.3241 4.25 15.5833 4.25C21.8426 4.25 26.9167 9.3241 26.9167 15.5833Z"
+                stroke="#00B8F1"
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
             </svg>
             <input
               className=" pl-[24px] mt-1 items-center text-sm text-black outline-none rounded-lg dark:bg-primary-white  dark:placeholder-primary dark:placeholder:text-xl dark:placeholder:font-medium"
