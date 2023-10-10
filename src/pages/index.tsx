09b0a190f91a285752a4c66997959adec9c4af64
maramzan <arsalanramzan811@gmail.com>
2023-10-10 16:45:27 +0500

    title changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,22 @@
-import * as React from "react"
+import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import Home from "./home";
 
-
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <div>
-      <Home/>
+      <Home />
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 export function Head() {
   return (
     <>
-      <title>TopSol Portfolio</title>
+      <title>TOPSOL Innovative Solution</title>
       <meta name="description" content="TopSol Portfolio " />
       <link rel="icon" href="/topsol-favicon.png" type="image/png"></link>
     </>
