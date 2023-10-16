ab2f8bd634be19c7a11231d9fb9d536d74b7309f
maramzan <arsalanramzan811@gmail.com>
2023-10-16 17:09:50 +0500

    adding the blogs on blog page

--- src/pages/blogs/blogDetail/components/Hero.tsx
+++ src/pages/blogs/blogDetail/components/Hero.tsx
@@ -9,12 +9,8 @@ export default function Hero({ data }: Iprops) {
       <div className=" pb-[50px] bg-gradient-to-b from-primary-gradientOne to-primary-gradientTwo">
         <div className="md:w-[80%] min-h-[100vh] md:container md:mx-auto md:flex-row flex flex-col  justify-center items-center">
           <div className="w-full mt-[70px]  md:container md:mx-auto md:mt-0  flex flex-col justify-center text-center md:text-left ">
-            <h1 className="text-[36px] md:text-[70px] xl:text-[90px]  text-white font-[600]">
-              What is <br />
-              <span className="md:inline xl:w-[100%] text-primary">Data </span>
-              <span className="inline md:block text-primary md:text-white">
-                Science
-              </span>
+            <h1 className="text-[36px] md:text-[70px] xl:text-[80px] break-words text-white font-[600]">
+              {data.title}
             </h1>
           </div>
           <div className="w-[100%] mt-[56px] md:mt-0 md:container justify-center items-center flex flex-col">
