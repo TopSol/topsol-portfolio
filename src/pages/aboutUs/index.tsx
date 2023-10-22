176921cdd6945c53f464b467182467155da0a511
maramzan <arsalanramzan811@gmail.com>
2023-10-22 16:41:33 +0500

    about us data added

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -6,13 +6,7 @@ import Footer from "../../components/footerSection";
 import InfoSection from "../../components/infoSection";
 import AddressSection from "../../components/addressSection";
 import Experience from "./component/Experience";
-
-const aboutUsData = {
-  heading: "Work with the Tech Leader.",
-  mainHeading: "Topsol brings ideas to life, leveraging modern technologies.",
-  initialText:
-    "Founded 2020, Topsol's evolution from a compact team to a dynamic mobile-first software development ompany showcases our commitment. With industry-leading team satisfaction, recognized by analysts, and a growing lient roster including, we're honored to have supported startups and distinguished brands alike.",
-};
+import aboutUsData from "../../staticData/aboutusData";
 
 const aboutUs = () => {
   return (
---
+++ src/staticData/aboutusData.ts
@@ -0,0 +1,7 @@
+const aboutUsData = {
+  heading: "Work with the Tech Leader.",
+  mainHeading: "Topsol brings ideas to life, leveraging modern technologies.",
+  initialText:
+    "Founded 2020, Topsol's evolution from a compact team to a dynamic mobile-first software development ompany showcases our commitment. With industry-leading team satisfaction, recognized by analysts, and a growing lient roster including, we're honored to have supported startups and distinguished brands alike.",
+};
+export default aboutUsData;
