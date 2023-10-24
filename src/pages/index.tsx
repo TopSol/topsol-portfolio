d39cac6a78f0ccc110898262199d0a95ffcb3b78
Asif-chohan <rmasifchohan@gmail.com>
2023-10-24 16:35:29 +0500

    meta tags added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,29 @@ export function Head() {
   return (
     <>
       <title>TOPSOL Innovative Solution</title>
-      <meta name="description" content="TopSol Portfolio " />
-      <link rel="icon" href="/topsol-favicon.png" type="image/png"></link>
+      <meta
+        name="description"
+        content="Topsol Innovative Solutions leads the way in cutting-edge technology, offering creative, tailored solutions to address complex challenges, driving progress across diverse industries."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://topsol.org/" />
+      <meta property="og:title" content="TOPSOL Innovative Solution" />
+      <meta
+        property="og:description"
+        content="Topsol Innovative Solutions leads the way in cutting-edge technology, offering creative, tailored solutions to address complex challenges, driving progress across diverse industries."
+      />
+      <meta property="og:image" content="/icons/logo.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://topsol.org/" />
+      <meta property="twitter:title" content="TOPSOL Innovative Solution" />
+      <meta
+        property="twitter:description"
+        content="Topsol Innovative Solutions leads the way in cutting-edge technology, offering creative, tailored solutions to address complex challenges, driving progress across diverse industries."
+      />
+      <meta property="twitter:image" content="/icons/logo.png" />
+      <link rel="icon" href="/icons/icon.png" type="image/png"></link>
     </>
   );
 }
