dd07ca2630d6f3c3c3e672d0a88b11d32e5b4cf9
maramzan <arsalanramzan811@gmail.com>
2023-12-18 13:04:55 +0500

    chaning card data and design

--- src/staticData/ratingeSectionData.ts
+++ src/staticData/ratingeSectionData.ts
@@ -6,28 +6,57 @@ import Juan from "../images/homeImages/ratingSection/juan.webp";
 
 const reviews = [
   {
-    text: "I recently had the pleasure of working with the software company, TopSol Software Solutions. They were extremely professional and easy to work with. The team was extremely knowledgeable and went out of their way to make sure that all of our needs were met. They always made sure to keep us updated on the progress of our project, and the final product was exactly what we were looking for. I would highly recommend TopSol Software Solutions for any software development project. They are reliable, efficient, and will provide exceptional customer service.",
-    clientName: "Juan Camilo",
-    type: "Founder Otherwise",
+    text: '“Sohaib is a mature software developer with methodical, pragmatic and balanced approach to software design and development. He’s easy to work with and always willing to put in extra effort during deadlines. Very recommend him! We will hire him again for another project.”',
+    clientName: "Jonas Silver",
+    type: "Founder HSC",
     avatar: Juan,
+    link:'https://www.upwork.com/freelancers/~017d99e42769833c38'
   },
   {
-    text: "I've been using the software from TopSol for quite some time now and I'm very impressed. The software is reliable and easy to use, and the customer service team is always available to help out with any issues or questions. I've had great success with their software and would highly recommend it to anyone looking for a quality solution.",
-    clientName: "Nick Dudaj",
-    type: "Co-Founder Life Mib",
+    text: "“Ali took over an exciting project and was extremely fast in understanding the code. Very good communication skills and deep technical ones. Can only recommend him and will continue working with him.”",
+    clientName: "Markus Becher",
+    type: "Founder 89steps",
     avatar: Nick,
+    link:'https://www.upwork.com/freelancers/~017d99e42769833c38'
   },
   {
-    text: "I recently had the pleasure of working with the software company, TopSol Software Solutions. They were extremely professional and easy to work with. The team was extremely knowledgeable and went out of their way to make sure that all of our needs were met. They always made sure to keep us updated on the progress of our project, and the final product was exactly what we were looking for. I would highly recommend TopSol Software Solutions for any software development project. They are reliable, efficient, and will provide exceptional customer service.",
+    text: "“Sohaib is an amazing Front End Developer. How completed his project successfully and was professional throughout the process. I would highly recommend him!”",
+    clientName: "Mohammad Umer",
+    type: "Founder Gigsasa",
+    avatar: Juan,
+    link:'https://www.upwork.com/freelancers/~017d99e42769833c38'
+  },
+  {
+    text: "Arsalan is a talented developer in his field. He quickly picks up the needs of his clients and is easy to work with. He managed to implement quick fixes to my ReactJs website and orchestrated the global redux store for data keeping. Very responsive and has great attention to details. I would love to work with him again and definitely recommend him to anyone who needs proper website development or assistance with website code issues.",
+    clientName: "Vladimir B Kagarlitskiy",
+    type: "Founder",
+    avatar: Chris,
+    link:'https://www.upwork.com/freelancers/~013b22c7c852e9eda0'
+
+  },
+  {
+    text: "Our experience with Hamza was excellent. He understands requirements quickly and clearly, delivers the features on time and on par with required quality. Writes code at the senior level and knows inside outs of front-end and back-end. With excellent research, debugging skills, he nailed down some of the crucial bugs. Hamza is a reliable, senior engineer and this guy knows what he's doing. If you want stuff get done with no BS!, hire Hamza as I highly recommend him. We'll hire him every time!",
+    clientName: "Fahad Majeed",
+    type: "Founder Apperative solution",
+    avatar: Chris,
+    link:'https://www.upwork.com/freelancers/~0158775381cc75937b'
+
+  },
+  {
+    text: "Asif is well skilled with Javascript, Typescript, React and Node.js. This contract was completed successfully and with satisfaction. The applications went into production without troubles and Asif was very responsive during the contract and afterwards. I recommend Asif for future projects and I will be working with him again.",
     clientName: "Juan Camilo",
     type: "Founder Otherwise",
-    avatar: Juan,
+    avatar: Chris,
+    link:'https://www.upwork.com/freelancers/~01a432094b689bdd42'
+
   },
   {
-    text: "I had the pleasure of working with TopSol company who developed mobile applications and I could not be more impressed with the results. Their team is highly professional, knowledgeable and they delivered a great product in a very timely manner. The mobile applications they developed were well designed, easy to use and met our exact requirements. The customer support was also top notch and they were always available to answer our questions and provide helpful advice. I highly recommend this software company for anyone looking for mobile application development services.",
-    clientName: "Christopher Burnette",
-    type: "Co-Founder, Developer & Manager @ Working Title Simulations",
+    text: "Asif is a valued contractor of Life Mib. We are grateful to have him, and we plan on hiring him again in the future.",
+    clientName: "Nick Dudaj",
+    type: "Founder Life Mib",
     avatar: Chris,
+    link:'https://www.upwork.com/freelancers/~01a432094b689bdd42'
+
   },
 ];
 
